5de5acb6ac46e1a4c1d7b0599a7c7360b70e37c2
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-08-03 02:18:16 -0400

    adjusted server and trying to troubleshoot module issue

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,8 +37,8 @@ const typeDefs = gql`
     }
 
   type Mutation {
-    addUser(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     saveBook(bookData: BookInput!): User
     removeBook: (bookId: ID!): User
  
